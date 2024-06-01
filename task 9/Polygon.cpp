d62277e84c395591ac3f179ae3d261a595e26d68
WasiliWilson <bed-com-45-21@unima.ac.mw>
2024-06-01 03:17:38 +0200

    lab task 9

---
+++ task 9/Polygon.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "Polygon.h"
+
+void Polygon::SetValues(int width, int height)
+{
+    mWidth = width;
+    mHeight = height;
+}
+
+//Adding new lines of code
+int Polygon::Area()
+{
+    return 0;
+}
---
+++ task 9/Polygon3.cpp
@@ -0,0 +1,15 @@
+#include "Polygon.h"
+#include <iostream>
+
+using namespace std;
+
+void Polygon::SetValues(int width, int height)
+{
+ mWidth = width;
+ mHeight = height;
+}
+
+void Polygon::PrintArea()
+{
+ cout << this->area() << '\n';
+}
